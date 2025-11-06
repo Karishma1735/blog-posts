9fb5e19bf76490b83702480e4fff4ee1a93dc61d
Karishma17gammaedge <karishma.saklecha@gammaedge.io>
2025-11-06 18:13:54 +0530

    fixing loader css

--- src/components/BlogsCard.jsx
+++ src/components/BlogsCard.jsx
@@ -84,10 +84,10 @@ const BlogsCard = () => {
         </div>
       </div>
       {loading ? (
-        <div className='bg-black h-screen '><div className=' p-10'>
-          <span className="loader"></span>
-          <p className='text-white'>Loading...</p>
-        </div></div>) : (
+       <div className="bg-black h-screen flex flex-col items-center justify-center ">
+    <span className="loader "></span>
+    <p className="text-white mt-6 text-lg tracking-wide">Loading...</p>
+  </div>) : (
         <div className="flex flex-wrap justify-center gap-8 p-8">
           {currentPosts.map((article, index) => {
             // const isFavourites = favourites.includes(article);
@@ -95,50 +95,6 @@ const BlogsCard = () => {
 
 
             return (
-              //     <div
-              //         key={index}
-              //         className="bg-white shadow-lg rounded-lg flex flex-col cursor-pointer w-100"
-              //     >
-              //         <img
-              //           src={
-              //                 article?.urlToImage ||
-              //                 "https://www.leblogauto.com/wp-content/uploads/2025/10/Screenshot-2025-10-29-13.03.30-1.png"
-              //             }
-              //             alt="No image found"
-              //             className="w-full h-60 object-cover rounded-t-lg"
-              //             onClick={() => navigate(`/details/${index}`, { state: { article } })}
-              //         />
-              //     <div className="p-4 flex flex-col">
-              //             <p className="font-bold mb-2">{article.title.slice(0, 60)}</p>
-              //             <p className="text-gray-700 ">
-              //                 {article?.description?.slice(0, 100) || "No description found"}
-              //             </p>
-              //  </div>
-
-              //      <div className="flex items-center justify-between px-4 pb-4">
-              //             <div className="flex items-center gap-3">
-              //                 <img
-              //                     src="https://i.pravatar.cc/40?img=5"
-              //                     alt="author"
-              //                     className="w-10 h-10 rounded-full object-cover"
-              //                 />
-              //                 <div>
-              //                     <p className="text-sm font-semibold">{article.author || "Unknown"}</p>
-              //                     <p className="text-xs text-gray-500">
-              //                         {new Date(article.publishedAt).toDateString()}
-              //                     </p>
-              //                 </div>
-              //             </div>
-
-              //  <FaStar
-              //                 onClick={() => handleFavourite(article)}
-              //                 className={`${
-              //                     isFavourites ? "text-yellow-400" : "text-black"
-              //    } text-2xl cursor-pointer`}
-              //             />
-              //         </div>
-              //     </div>
-
               <Card
                 key={index}
                 article={article}
--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -6,43 +6,45 @@ import Card from './Card';
 
 function Favourites() {
     const dispatch = useDispatch()
-  const favourites = useSelector((state) => state.favourites.items);
-  console.log(favourites);
+    const favourites = useSelector((state) => state.favourites.items);
+    console.log(favourites);
     const handleFavourite = (article) => {
         dispatch(togglefavourites(article))
 
-      }
-  
-  return (
-    <div className='bg-sky-100 h-screen'>
-    <div className="flex flex-wrap justify-center gap-8 p-4 ">
+    }
 
-        {favourites.length===0?
-        
-        <div>
+    return (
+        <div className='bg-sky-100 h-screen'>
+            <div className="flex flex-wrap justify-center gap-8 p-4 ">
 
-            <p className='text-4xl font-bold '>U have not add any favourites yet</p>
-        </div>:(<div className="flex flex-wrap justify-center item-center gap-8 p-4">
+                {favourites.length === 0 ?
+                    <div className="w-full">
+                        <p className="text-3xl  font-semibold text-center text-gray-700">
+                            No items in Favourites
+                        </p>
+                    </div>
+                    :
+                    (<div className="flex flex-wrap justify-center item-center gap-8 p-4">
 
-              {favourites.map((article, index) => {
+                        {favourites.map((article, index) => {
                             // const isFavourites = favourites.includes(article);
                             const isFavourites = favourites.some(fav => fav.url === article.url);
-    
-    
+
+
                             return (
-                             <Card
-                key={index}
-                article={article}
-                isFavourites={isFavourites}
-                onFavouriteToggle={handleFavourite}
-              />
+                                <Card
+                                    key={index}
+                                    article={article}
+                                    isFavourites={isFavourites}
+                                    onFavouriteToggle={handleFavourite}
+                                />
                             );
                         })}
-        </div>)}
-                      
-                    </div>
-                    </div>
-  )
+                    </div>)}
+
+            </div>
+        </div>
+    )
 }
 
 export default Favourites
